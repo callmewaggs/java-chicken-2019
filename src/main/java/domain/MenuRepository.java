f91ef281f07faff40f058f96a67b729f3fd440f2
callmewaggs <zxv74014@gmail.com>
2020-02-28 18:29:53 +0900

    feat: 메뉴저장소에 기능 추가
     - 입력으로 들어온 메뉴 번호와 일치하는 메뉴를 찾아주는 기능 추가

--- src/main/java/domain/MenuRepository.java
+++ src/main/java/domain/MenuRepository.java
@@ -5,20 +5,28 @@ import java.util.Collections;
 import java.util.List;
 
 public class MenuRepository {
-    private static final List<Menu> menus = new ArrayList<>();
 
-    static {
-        menus.add(new Menu(1, "후라이드", Category.CHICKEN, 16_000));
-        menus.add(new Menu(2, "양념치킨", Category.CHICKEN, 16_000));
-        menus.add(new Menu(3, "반반치킨", Category.CHICKEN, 16_000));
-        menus.add(new Menu(4, "통구이", Category.CHICKEN, 16_000));
-        menus.add(new Menu(5, "간장치킨", Category.CHICKEN, 17_000));
-        menus.add(new Menu(6, "순살치킨", Category.CHICKEN, 17_000));
-        menus.add(new Menu(21, "콜라", Category.BEVERAGE, 1_000));
-        menus.add(new Menu(22, "사이다", Category.BEVERAGE, 1_000));
-    }
+  private static final List<Menu> menus = new ArrayList<>();
 
-    public static List<Menu> menus() {
-        return Collections.unmodifiableList(menus);
-    }
+  static {
+    menus.add(new Menu(1, "후라이드", Category.CHICKEN, 16_000));
+    menus.add(new Menu(2, "양념치킨", Category.CHICKEN, 16_000));
+    menus.add(new Menu(3, "반반치킨", Category.CHICKEN, 16_000));
+    menus.add(new Menu(4, "통구이", Category.CHICKEN, 16_000));
+    menus.add(new Menu(5, "간장치킨", Category.CHICKEN, 17_000));
+    menus.add(new Menu(6, "순살치킨", Category.CHICKEN, 17_000));
+    menus.add(new Menu(21, "콜라", Category.BEVERAGE, 1_000));
+    menus.add(new Menu(22, "사이다", Category.BEVERAGE, 1_000));
+  }
+
+  public static List<Menu> menus() {
+    return Collections.unmodifiableList(menus);
+  }
+
+  public static Menu findMenuByNumber(int number) {
+    return menus().stream()
+        .filter(menu -> menu.getNumber() == number)
+        .findFirst()
+        .orElseThrow(() -> new IllegalArgumentException("일치하는 메뉴가 없습니다. 다시 시도해 주세요."));
+  }
 }
---
+++ src/test/java/domain/MenuRepositoryTest.java
@@ -0,0 +1,47 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuRepositoryTest {
+
+  @DisplayName("메뉴 번호에 해당하는 메뉴를 찾는다.")
+  @Test
+  public void find_menu_by_number() {
+    // Arrange
+    int number = 1;
+
+    // Act
+    Menu actual = MenuRepository.findMenuByNumber(number);
+
+    // Assert
+    assertNotNull(actual);
+  }
+
+  @DisplayName("메뉴 번호에 해당하는 메뉴가 없다면 예외를 던진다.")
+  @Test
+  public void exception_thrown_if_there_is_no_matched_menu_when_find_menu_by_number() {
+    // Arrange
+    int nonExistMenuNumber = -1;
+
+    // Act
+    IllegalArgumentException actual =
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> MenuRepository.findMenuByNumber(nonExistMenuNumber));
+
+    // Assert
+    assertEquals("일치하는 메뉴가 없습니다. 다시 시도해 주세요.", actual.getMessage());
+  }
+
+  @Test
+  public void menus_test() {
+    List<Menu> menus = MenuRepository.menus();
+    assertEquals(8, menus.size());
+  }
+}
