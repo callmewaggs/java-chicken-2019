2e57b56ef45a79536c7fd38b850b23ee251f428e
callmewaggs <zxv74014@gmail.com>
2020-02-28 17:53:54 +0900

    feat: 메뉴에 static 필드 및 기능 추가
     - 테이블 당 한 메뉴의 최대 수량을 나타내는 static 필드 추가
     - getter 메서드 추가
     - 테스트 코드 추가

--- src/main/java/domain/Menu.java
+++ src/main/java/domain/Menu.java
@@ -1,20 +1,39 @@
 package domain;
 
 public class Menu {
-    private final int number;
-    private final String name;
-    private final Category category;
-    private final int price;
 
-    public Menu(final int number, final String name, final Category category, final int price) {
-        this.number = number;
-        this.name = name;
-        this.category = category;
-        this.price = price;
-    }
+  public static int MAX_ORDER_QUANTITY_PER_TABLE = 99;
 
-    @Override
-    public String toString() {
-        return category + " " + number + " - " + name + " : " + price + "원";
-    }
+  private final int number;
+  private final String name;
+  private final Category category;
+  private final int price;
+
+  public Menu(final int number, final String name, final Category category, final int price) {
+    this.number = number;
+    this.name = name;
+    this.category = category;
+    this.price = price;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  @Override
+  public String toString() {
+    return category + " " + number + " - " + name + " : " + price + "원";
+  }
 }
---
+++ src/test/java/domain/MenuTest.java
@@ -0,0 +1,43 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuTest {
+
+  @DisplayName("메뉴는 번호, 이름, 카테고리, 가격이 입력되어야 생성된다.")
+  @Test
+  public void create_menu_test() {
+    // Arrange
+    int number = 1;
+    String name = "메뉴";
+    Category category = Category.CHICKEN;
+    int price = 10_000;
+
+    // Act
+    Menu actual = new Menu(number, name, category, price);
+
+    // Assert
+    assertEquals(number, actual.getNumber());
+    assertEquals(name, actual.getName());
+    assertEquals(category, actual.getCategory());
+    assertEquals(price, actual.getPrice());
+  }
+
+  @Test
+  public void toString_test() {
+    // Arrange
+    int number = 1;
+    String name = "메뉴";
+    Category category = Category.CHICKEN;
+    int price = 10_000;
+
+    // Act
+    Menu actual = new Menu(number, name, category, price);
+
+    // Assert
+    assertEquals("[치킨] 1 - 메뉴 : 10000원", actual.toString());
+  }
+}
