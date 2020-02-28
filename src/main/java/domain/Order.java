b63458f9b38acafa613807d0e756c311e3a18f59
callmewaggs <zxv74014@gmail.com>
2020-02-28 18:33:14 +0900

    feat: 주문 도메인 생성
     - mockito 의존성 추가
     - README.md 의 기능 목록 수정

---
+++ src/main/java/domain/Order.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Order {
+
+  private Menu menu;
+  private int quantity;
+
+  public Order(Menu menu, int quantity) {
+    this.menu = menu;
+    this.quantity = quantity;
+  }
+
+  public Menu getMenu() {
+    return menu;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+}
---
+++ src/test/java/domain/OrderTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OrderTest {
+
+  @DisplayName("주문은 메뉴와 수량을 입력받아 생성된다.")
+  @Test
+  public void order_created_with_menu_and_quantity() {
+    // Arrange
+    Menu menu = mock(Menu.class);
+    int quantity = 1;
+
+    // Act
+    Order order = new Order(menu, quantity);
+
+    // Assert
+    assertNotNull(order.getMenu());
+    assertEquals(1, order.getQuantity());
+  }
+}
